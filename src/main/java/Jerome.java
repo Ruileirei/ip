31ad59a4a7b9a75a577c4a84beccd84ba508d8ce
Ruileirei <angruiwenray@gmail.com>
2025-08-20 11:09:19 +0800

    Add support for list, adding to list

--- src/main/java/Jerome.java
+++ src/main/java/Jerome.java
@@ -1,17 +1,37 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Jerome {
     public static void main(String[] args) {
         String line = "_____________________________";
+        ArrayList<String> ls = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
+
         //introduction
         System.out.println(line + "\nHello, I'm Jerome!\n" +  "What can I do for you?\n" + line);
 
         //user input
-        String response = sc.next();
+        String response = sc.nextLine();
+
+        //Responses
         while (!response.equals("bye")) {
-            System.out.println(line + "\n" +response + "\n" + line);
-            response = sc.next();
+            if (response.equals("list")) {
+                //if task list is empty
+                if (ls.size() == 0) {
+                    System.out.println("There is no tasks currently :/");
+                } else {
+                    //print all tasks if list is not empty
+                    System.out.println("Here are you tasks: ");
+                    for (String e : ls) {
+                        System.out.println(ls.indexOf(e) + 1 + ". " + e);
+                    }
+                }
+            } else {
+                //add task, echo
+                ls.add(response);
+                System.out.println(line + "\n" + "added: " + response + "\n" + line);
+            }
+            response = sc.nextLine();
         }
 
         //ending
