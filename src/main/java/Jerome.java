4e874d92dcc13f2c8d278e5ff13f997a192ff25b
Ruileirei <angruiwenray@gmail.com>
2025-08-20 10:26:03 +0800

    Renamed main class, added greeting and exit

---
+++ src/main/java/Jerome.java
@@ -0,0 +1,9 @@
+public class Jerome {
+    public static void main(String[] args) {
+        String line = "_____________________________";
+        //introduction
+        System.out.println(line + "\nHello, I'm Jerome!\n" +  "What can I do for you?\n" + line);
+        //ending
+        System.out.println("Bye. Hope to see you soon!\n" + line);
+    }
+}
