647ea013a70e62f6a0e47b11f3dde121d44d2573
Ruileirei <angruiwenray@gmail.com>
2025-08-20 11:52:44 +0800

    Create Task class, Add support for Marking, Unmarking tasks

--- src/main/java/Jerome.java
+++ src/main/java/Jerome.java
@@ -4,36 +4,58 @@ import java.util.ArrayList;
 public class Jerome {
     public static void main(String[] args) {
         String line = "_____________________________";
-        ArrayList<String> ls = new ArrayList<>();
+        ArrayList<Task> ls = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         //introduction
         System.out.println(line + "\nHello, I'm Jerome!\n" +  "What can I do for you?\n" + line);
 
-        //user input
-        String response = sc.nextLine();
-
         //Responses
-        while (!response.equals("bye")) {
-            if (response.equals("list")) {
+        while (true) {
+            //user input
+            String response = sc.nextLine();
+            if (response.equals("bye")) {
+                break;
+            }
+            if (response.isEmpty()) {
+                //ignore empty responses
+                continue;
+            } else if (response.equals("list")) {
                 //if task list is empty
-                if (ls.size() == 0) {
+                if (ls.isEmpty()) {
                     System.out.println("There is no tasks currently :/");
                 } else {
                     //print all tasks if list is not empty
                     System.out.println("Here are you tasks: ");
-                    for (String e : ls) {
-                        System.out.println(ls.indexOf(e) + 1 + ". " + e);
+                    for (int i = 0; i < ls.size(); i++) {
+                        System.out.println((i + 1) + ". " + ls.get(i));
                     }
+                }
+            } else if (response.startsWith("unmark") || response.startsWith("mark")) {
+                //marking and unmarking a task
+                String[] input = response.split(" ");
+                try {
+                    int ind = Integer.parseInt(input[1]) - 1;
+                    if (ind < 0 || ind >= ls.size()) {
+                        System.out.println("Sorry! I can't find that task :(");
+                        continue;
+                    }
+                    if (response.contains("unmark")) {
+                        ls.get(Integer.parseInt(input[1]) - 1).unmark();
+                    } else {
+                        ls.get(Integer.parseInt(input[1]) - 1).mark();
+                    }
+                } catch (NumberFormatException e) {
+                    //if input is mark __ where it is not an integer.
+                    System.out.println("Sorry! I can't find that task :(");
                 }
             } else {
                 //add task, echo
-                ls.add(response);
+                Task task = new Task(response);
+                ls.add(task);
                 System.out.println(line + "\n" + "added: " + response + "\n" + line);
             }
-            response = sc.nextLine();
         }
-
         //ending
         System.out.println("Bye. Hope to see you soon!\n" + line);
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public void mark() {
+        this.completed = true;
+        System.out.println("Nice! I have marked the following task as complete!");
+        System.out.println(this);
+    }
+
+    public void unmark() {
+        this.completed = false;
+        System.out.println("Aww! I will unmark the following task...");
+        System.out.println(this);
+    }
+
+    public String getStatus() {
+        return (completed ? "x"
+                           : " ");
+    }
+
+    @Override
+    public String toString() {
+        String str = "[" + this.getStatus() + "] " + this.description;
+        return str;
+    }
+}
