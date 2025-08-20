8397019904930df028bf201ab88a74cd338f1e62
Ruileirei <angruiwenray@gmail.com>
2025-08-20 10:40:52 +0800

    Add support for echo

--- src/main/java/Jerome.java
+++ src/main/java/Jerome.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class Jerome {
     public static void main(String[] args) {
         String line = "_____________________________";
+        Scanner sc = new Scanner(System.in);
         //introduction
         System.out.println(line + "\nHello, I'm Jerome!\n" +  "What can I do for you?\n" + line);
+
+        //user input
+        String response = sc.next();
+        while (!response.equals("bye")) {
+            System.out.println(line + "\n" +response + "\n" + line);
+            response = sc.next();
+        }
+
         //ending
         System.out.println("Bye. Hope to see you soon!\n" + line);
+
     }
 }
